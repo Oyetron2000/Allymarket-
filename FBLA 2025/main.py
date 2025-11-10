4fcde28545e43df0bdc83ee39ab2f5eb4729e8d1
dyldude6543 <amdycarballo9@gmail.com>
2025-11-10 02:45:14 -0500

    Update main.py

--- FBLA 2025/main.py
+++ FBLA 2025/main.py
@@ -4,8 +4,16 @@ from Website import create_app
 
 app = create_app()
 
+# --- TEMPORARY ROUTE: create all database tables on Render ---
+@app.route("/init-db")
+def init_db():
+    from Website import db
+    db.create_all()
+    return "Database initialized successfully!"
+
 #the debug = True means that any change i make in pythin right now automatically updates to the website as well
 #when it goes live, i should turn it off
 
 if __name__ == '__main__':
+
     app.run(debug =True )
