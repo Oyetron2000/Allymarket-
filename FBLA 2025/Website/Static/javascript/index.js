c99f35ae326464966a1f926c9abb92284c6c86c8
dyldude6543 <amdycarballo9@gmail.com>
2025-11-11 00:25:16 -0500

    Update index.js

--- FBLA 2025/Website/Static/javascript/index.js
+++ FBLA 2025/Website/Static/javascript/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function deleteNote(noteId) {
   fetch("/delete-note", {
     method: "POST",
@@ -7,3 +8,14 @@ function deleteNote(noteId) {
   });
 }
 
+=======
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+
+>>>>>>> f19af0ed54e0121dabf9f028d812f93686dc39d9
