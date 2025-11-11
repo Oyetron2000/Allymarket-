cf354ae3c778ae2ae64a0eb10194e9998aae9dff
dyldude6543 <amdycarballo9@gmail.com>
2025-11-10 23:03:25 -0500

    Update __init__.py

--- FBLA 2025/Website/__init__.py
+++ FBLA 2025/Website/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__, static_folder='Static', template_folder='Templates')
     app.config['SECRET_KEY'] = '1 luv @ppl3s'
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static/uploads/profiles')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(app.root_path, "instance", DB_NAME)}'
     db.init_app(app)
 
     from .view import views
@@ -38,4 +38,5 @@ def create_app():
 def create_database(app):
     with app.app_context():
         db.create_all()
+
 
